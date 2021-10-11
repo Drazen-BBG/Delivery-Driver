1073fb604f67f671094af915e607a562bdb39393
Drazen Drinic <drazendrinic@hotmail.com>
2021-10-11 23:46:45 +0200

    Delivery conditions

--- Delivery Driver/Assets/Scripts/Player Scripts/Delivery.cs
+++ Delivery Driver/Assets/Scripts/Player Scripts/Delivery.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool hasPackage = false;
+
+    [SerializeField] float destrayDelay = 0.2f;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("You Collided, Ouch!!!");
@@ -11,16 +15,17 @@ public class Delivery : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Package")
+        if (collision.tag == "Package" && !hasPackage)
         {
             Debug.Log("Package picked up!");
-
+            hasPackage = true;
+            Destroy(collision.gameObject, destrayDelay);
         }
         
-        if (collision.tag == "Customer")
+        if (collision.tag == "Customer" && hasPackage)
         {
             Debug.Log("Package delivered!");
-
+            hasPackage = false;
         }
     }
 }
